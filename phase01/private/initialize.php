b18a6f49b753f9ea60fe654f30bbf538e04e299c
Charlie Wallin <charliekwallin@gmail.com>
2024-02-20 11:00:43 -0500

    Fixed initialize.

--- phase01/private/initialize.php
+++ phase01/private/initialize.php
@@ -16,9 +16,10 @@
   // define("WWW_ROOT", '/~kevinskoglund/globe_bank/public');
   // define("WWW_ROOT", '');
   // * Can dynamically find everything in URL up to "/public"
-  $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
+  $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') +7;
   $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
   define("WWW_ROOT", $doc_root);
+
   
   require_once('functions.php');
 
